a376a04cc0f2f6fc4fb4484f8f7e3b108d2161d7
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-07 19:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.flex');
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function handleMove(e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const playbackRate = percent * (max - min) + min;
+  const height = Math.round(percent * 100) + '%';
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + 'x';
+  video.playbackRate = playbackRate;
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+speed.addEventListener('mousemove', handleMove);
